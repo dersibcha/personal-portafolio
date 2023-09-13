afe336298cc3e275c6b74a0cdb7de98f37b71ed1
Derian Felipe Sibaja Chavarría <derfelsib@gmail.com>
2023-09-12 20:10:23 -0600

    fix: image mobile about

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,7 +4,7 @@ import Education from "./Education";
 import Experience from "./Experience";
 import PersonalInfo from "./PersonalInfo";
 import Skills from "./Skills";
-import heroImgMobile from "../../../public/assets/img/hero/img-mobile.jpg";
+import heroImgMobile from "../../../public//assets/img/hero/pixelgif.gif"
 import Image from "next/image";
 import useDownloader from "react-use-downloader";
 
